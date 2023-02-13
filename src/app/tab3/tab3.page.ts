1386cdbb94658c7e661962b387f8b0a6095836d1
S3BASCH <sebaschuquiguanga@gmail.com>
2023-02-13 11:39:01 -0500

    mclovin

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  styleUrls: ['style.css']
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor(private alertController: AlertController) {}
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Important message',
+      message: 'Esta credencial es falsa',
+      buttons: ['OK'],
+    });
 
+    await alert.present();
+  }
 }
